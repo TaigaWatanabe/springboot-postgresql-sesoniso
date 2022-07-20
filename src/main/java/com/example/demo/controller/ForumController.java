15b5ccdcae17ee9172430f1ec00fbbc4c5cc4944
Taiga Watanabe <64018398+TaigaWatanabe@users.noreply.github.com>
2022-07-20 16:33:00 +0900

    change URL

--- src/main/java/com/example/demo/controller/ForumController.java
+++ src/main/java/com/example/demo/controller/ForumController.java
@@ -63,7 +63,7 @@ public class ForumController {
 		List<Comment> commentFilterdData = commentService.findAllCommentByUserId(userId);
 
 		// Set a top page
-		mav.setViewName("top");
+		mav.setViewName("op");
 		// Store an object
 		mav.addObject("UsersPosts", UsersPosts);
 		mav.addObject("userInfo",userData);
